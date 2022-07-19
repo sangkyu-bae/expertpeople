7f6ce022a3e8c5e8449626c516a1568b50f91cac
82108 <tkdrb1361@naver.com>
2022-07-19 19:53:11 +0900

    first commit

---
+++ src/main/java/com/expertpeople/ExpertpeopleApplication.java
@@ -0,0 +1,13 @@
+package com.expertpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertpeopleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpertpeopleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/expertpeople/ExpertpeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expertpeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertpeopleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
