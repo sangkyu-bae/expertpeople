d3d411dcfddf0e3257b7bd9c404787a923b73dfb
82108 <tkdrb1361@naver.com>
2022-07-20 20:14:49 +0900

    기본 설정 세팅

---
+++ src/main/java/com/expertpeople/modules/account/AccountService.java
@@ -0,0 +1,30 @@
+package com.expertpeople.modules.account;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AccountService implements UserDetailsService {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+       Account account=accountRepository.findByEmail(email);
+       if(account==null){
+           throw new UsernameNotFoundException(email);
+       }
+
+       return new UserAccount(account);
+    }
+}
